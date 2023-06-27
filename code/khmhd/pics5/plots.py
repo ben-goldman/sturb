ddc8e61d52421bb11ac50c01edceba9f65d6102c
ben-goldman <benogoldman@gmail.com>
2023-06-27 14:22:07 -0400

    part II

---
+++ code/khmhd/pics5/plots.py
@@ -0,0 +1,24 @@
+import numpy as np, matplotlib.pyplot as plt, h5py
+f = h5py.File("../MHD_w.h5")
+for t in range(1, 50):
+    fig, (ax1, ax2) = plt.subplots()
+    t = str(t*100)
+    u2 = np.array(f[f"UB0/3D/{t}"])**2 + np.array(f[f"UB1/3D/{t}"])**2 + np.array(f[f"UB2/3D/{t}"])**2
+    b2 = np.array(f[f"UB3/3D/{t}"])**2 + np.array(f[f"UB4/3D/{t}"])**2 + np.array(f[f"UB5/3D/{t}"])**2
+    c = plt.imshow(np.log(B2[:, :, 64]))
+    plt.colorbar(c)
+    plt.suptitle(f"$t={t}$")
+    fname = f"b2_{int(t):04d}.png"
+    plt.savefig(fname)
+    plt.close()
+    print(fname)
+
+for t in range(1, 50):
+    t = str(t*100)
+    c = plt.imshow(np.log(u2[:, :, 64]))
+    plt.colorbar(c)
+    plt.suptitle(f"$t={t}$")
+    fname = f"u2_{int(t):04d}.png"
+    plt.savefig(fname)
+    plt.close()
+    print(fname)
